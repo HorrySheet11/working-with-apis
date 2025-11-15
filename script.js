ab7cc6e16d7f3e71bb6f7033a5405da3069106c0
HorrySheet11 <marinnathan11@gmail.com>
2025-11-15 11:26:01 +0800

    added search input to look for different gif

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const img = document.querySelector("img");
+const search = document.querySelector("#search-input");
+let searchValue;
+const replaceSearch = (input) =>{
+	searchValue = input.replaceAll(" ", "-");
+}
 
-const fetchGIF =()=>(fetch(
-	"https://api.giphy.com/v1/gifs/translate?api_key=p5ENc6JElwzTQ1MZ3uPmBUsI9ZlraLpM&s=the_owl_house",
-)
-	.then((response) => {
-		return response.json();
-	})
-	.then((response) => {
-		img.src = response.data.images.original.url;
-	}));
+const fetchGIF = (search = "the_owl_house") =>
+	fetch(
+		`https://api.giphy.com/v1/gifs/translate?api_key=p5ENc6JElwzTQ1MZ3uPmBUsI9ZlraLpM&s=${search}`,
+	)
+		.then((response) => {
+			console.log(response);
+			return response.json();
+		})
+		.then((response) => {
+			img.src = response.data.images.original.url;
+		})
+		.catch((error) => {
+			console.log(`Error: ${error}`);
+		});
+
 fetchGIF();
-setInterval(fetchGIF, 5000);
-
-document.querySelector("button").addEventListener("click", fetchGIF);
+search.addEventListener("input",()=> {replaceSearch(search.value)});
+document.querySelector(".newGIF").addEventListener("click",() => fetchGIF(searchValue));
