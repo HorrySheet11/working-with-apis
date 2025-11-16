f174bc739ced3edc1309e49aa930adaa84745274
HorrySheet11 <marinnathan11@gmail.com>
2025-11-16 10:06:40 +0800

    implemented no repeat GIF
    made it so the next GIF will not be the current GIF

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const img = document.querySelector("img");
 const search = document.querySelector("#search-input");
 let searchValue;
-const replaceSearch = (input) =>{
+const replaceSearch = (input) => {
 	searchValue = input.replaceAll(" ", "-");
-}
+};
 
-const fetchGIF = (search = "the_owl_house") =>
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=p5ENc6JElwzTQ1MZ3uPmBUsI9ZlraLpM&s=${search}`,
-	)
-		.then((response) => {
-			console.log(response);
-			return response.json();
-		})
-		.then((response) => {
-			img.src = response.data.images.original.url;
-		})
-		.catch((error) => {
-			console.log(`Error: ${error}`);
-		});
+const fetchGIF = async (search = "the_owl_house") => {
+	try {
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=p5ENc6JElwzTQ1MZ3uPmBUsI9ZlraLpM&s=${search}`,
+		);
+		const data = await response.json();
+		console.log(data.data.images.original.url);
+		if (data.data.images.original.url.slice(-15) === img.src.slice(-15)){
+			fetchGIF(searchValue);
+		}else{
+			img.src = data.data.images.original.url;
+		}
+	} catch (error) {
+		console.log(`Error: ${error}`);
+	}
+};
 
 fetchGIF();
-search.addEventListener("input",()=> {replaceSearch(search.value)});
-document.querySelector(".newGIF").addEventListener("click",() => fetchGIF(searchValue));
+search.addEventListener("input", () => {
+	replaceSearch(search.value);
+});
+document
+	.querySelector(".newGIF")
+	.addEventListener("click", () => fetchGIF(searchValue));
